e5f7ca3fc6370ba6981c7ad720cf0106190a0be1
hamza <86945130+hmmmzaDev@users.noreply.github.com>
2025-01-25 17:59:22 +0500

    Update applications.nix - removed unnecessary apps

--- hydenix/packages/applications.nix
+++ hydenix/packages/applications.nix
@@ -8,9 +8,9 @@
     kdePackages.dolphin # kde file manager
     ark # kde file archiver
     vim # terminal text editor
-    vscode # ide text editor
-    discord # chat client
-    webcord # discord client
-    vesktop # discord client
+    #vscode # ide text editor
+    #discord # chat client
+    #webcord # discord client
+    #vesktop # discord client
   ];
 }
